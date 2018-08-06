5b43705f3e302a195191c317479d466101bb058c
ar-guzman <a.ricardoguzman@gmail.com>
2018-08-06 15:02:41 -0600

    test

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+(function(){
+
+  let btn = document.getElementById('practica1');
+
+  btn.addEventListener('click',()=>{
+      let vw = document.getElementById('window'),
+          div = document.createElement('div');
+      vw.innerHTML = `<label> Bryan hueco </label>`;
+      
+  });
+
+})()
