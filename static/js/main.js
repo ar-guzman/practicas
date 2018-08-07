ec1e6355268ef9707d612864d253739a6c5653cf
ar-guzman <a.ricardoguzman@gmail.com>
2018-08-07 12:36:09 -0600

    pagina proto

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,23 @@
 (function(){
+  
+    createElement = (tagName,properties)=>{
+        let el = document.createElement(tagName);
+        Object.keys(properties).forEach((item)=>{
+            if(item == 'textContent') 
+                el.textContent = properties[item];
+            else
+                el.setAttribute(item,properties[item]);
+        });
+        return el;
+    };
 
-  let btn = document.getElementById('practica1');
+    let btn = document.getElementById('about');
 
-  btn.addEventListener('click',()=>{
-      let vw = document.getElementById('window'),
-          div = document.createElement('div');
-      vw.innerHTML = `<label> Bryan hueco </label>`;
-      
-  });
+    btn.addEventListener('click',()=>{
+        let vw = document.getElementById('window'),
+            div = document.createElement('div');
+        vw.appendChild(createElement('h1',{'textContent':'Practica 1: Mantenimiento de una computadora.','class':'titulo'})); 
+        vw.appendChild(createElement('p',{'textContent':'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab architecto accusantium laboriosam repellat, eligendi tenetur accusamus, eveniet tempore at distinctio vel sint fugit illo, illum quibusdam tempora inventore nihil. Enim.'}));
+    });
 
 })()
