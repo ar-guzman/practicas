45cf1f6235183caa787dd0d8f2f116a7932b93fa
ar-guzman <a.ricardoguzman@gmail.com>
2018-08-22 01:45:34 -0600

    practica 2

--- static/js/main.js
+++ static/js/main.js
@@ -87,8 +87,8 @@
             modal.parentNode.classList.remove('hidden');
 
             switch(val){
-                case 1:
-                    renderTemplate(modal,
+              case 1:
+                  renderTemplate(modal,
                         {title:'PRACTICA1: MANTENIMIENTO DE UN EQUIPO DE COMPUTACION',
                         image: "./static/media/practica1-foto.jpeg",
                         instructions: ['Objetivos',
@@ -139,8 +139,67 @@
                     ]},'links');
                   modalBody.appendChild(newDocFrag.content.cloneNode(true));
                 break;
+              case 2:{
+                    renderTemplate(modal,
+                        {title:'PRACTICA2: INSTALACION DE DISTINTOS SOs Y COMUNICACIÓN ENTRE ESTOS',
+                        image: "./static/media/practica2-foto.jpeg",
+                        instructions: ['Objetivos',
+                                        `Que el estudiante plantee soluciones para la grabación y edición de video de escritorio.
+                                        Familiarizar al estudiante con el entorno Linux, instalando cualquiera de la amplia variedad de distribuciones
+                                        que este ofrece.
+                                        Plantear soluciones para instalar Linux, sin necesidad de eliminar por completo, el sistema operativo
+                                        Windows.
+                                        Que el estudiante investigue sobre las diferentes opciones que existen en Internet, para poder publicar el
+                                        video realizado.`,
+                                        'Descripción',
+                                        `Se elaboró un video-tutorial de "MANTENIMIENTO DE UNA COMPUTADORA". El video se encuentra en la parte de
+                                        abajo dentro de este mismo cuadro. En el video se explica como darle mantenimiento a un equipo Acer Aspire 3680/ZR1,
+                                        también están el manual y el trifoliar requeridos. Finalmente se encontrarán los links que nos sirvieron de guía para
+                                        realizar esta práctica.`,
+                                        'Manuales y desarrollo de la práctica'
+                                      ],},
+                          'blog-post');
+                      let divs = [createElement('div',{class:"section"}),createElement('div',{class:"section"}),createElement('div',{class:"section"})];
+                      divs[0].appendChild(createElement('button',{class:'show','textContent':'Mostrar Trifoliar PDF',url:"https://drive.google.com/file/d/11ZezDrcscWrlgLc5hoMPA_dXSRRViG9g/preview",index:1}));
+                      divs[1].appendChild(createElement('button',{class:'show','textContent':'Mostrar Manual PDF',url:"https://drive.google.com/file/d/1dKGUL7YdU3nySuZ8jTFPfDeZjQPV6XsX/preview",index:2}));
+                      divs[2].appendChild(createElement('button',{class:'show','textContent':'Mostrar Video',url:"https://www.youtube.com/embed/https://www.youtube.com/embed/jLDT6dF9MUE&?playlist=jLDT6dF9MUE&loop=1",index:3}));
+                      let modalBody = modal.querySelector('.modal-body');
+                      modalBody.appendChild(divs[0]);
+                      modalBody.appendChild(divs[1]);
+                      modalBody.appendChild(divs[2]);
+                      modalBody.querySelectorAll('button.show').forEach((item)=>{item.addEventListener('click',()=>{
+                        let index = Number(item.getAttribute('index'));
+                        if(item.classList.contains('clicked')){
+                          item.classList.remove('clicked');
+                          item.parentNode.querySelector('iframe').remove();
+                          item.parentNode.style.background = "";
+                          item.textContent = (index==2)?'Mostrar Manual PDF':(index==1)?'Mostrar Trifoliar PDF':'Mostrar Video';
+                        }
+                        else{
+                          item.classList.add('clicked');
+                          item.textContent = (index==3)?'Cerar Video':'Cerrar PDF';
+                          item.parentNode.insertBefore(createElement('iframe',{src:item.getAttribute('url')}),item.nextSibling);
+                          item.parentNode.style.background = "black";
+                        }
+                      });
+                    });
+                    let docFrag = document.createElement('template');
+                    renderTemplate(docFrag,{title:'Link a los documentos del grupo',links:[
+                      ["https://drive.google.com/open?id=11ZezDrcscWrlgLc5hoMPA_dXSRRViG9g",'Trifoliar en drive'],
+                      ["https://drive.google.com/open?id=1dKGUL7YdU3nySuZ8jTFPfDeZjQPV6XsX",'Manual de mantenimiento en drive'],
+                      ["https://youtu.be/jLDT6dF9MUE","Video de la practica"]
+                      ]},'links');
+                    modalBody.appendChild(docFrag.content.cloneNode(true));
+                    let newDocFrag = document.createElement('template');
+                    renderTemplate(newDocFrag,{title:'Link a los documentos y videos de apoyo',links:[
+                      ["https://www.youtube.com/watch?v=93RzkL6dKyE&t=64s",'Video para abrir la computadora Acer Aspire 3680/ZR1'],
+                      ["https://www.dell.com/support/article/gt/es/gtbsdt1/sln290711/mantenimiento-general-que-debe-realizar-para-hacer-mantenimiento-a-su-computadora-dell?lang=es",'Guía de mantenimiento general a una computadora'],
+                      ]},'links');
+                    modalBody.appendChild(newDocFrag.content.cloneNode(true));
+                  }
+                  break;
                 default:
-                break;
+                  break;
             }
         });
 
